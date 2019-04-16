f560ddbda8f071be4f63e0836c7418dd9cabea8d
Caroline <carolineho1983@yahoo.fr>
2019-04-16 14:02:55 +0100

    first commit

---
+++ Csharp_tuto_Giraffe/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Csharp_tuto_Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Bernardo";
+            int characterAge;
+            characterAge = 92;
+
+
+            Console.WriteLine("there once was a man named " + characterName);
+            Console.WriteLine("he was " + characterAge + " yrs old");
+
+            characterName = "Bobby Brian";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("but didn't like being "  + characterAge);
+
+            Console.ReadLine();
+        }  
+    }
+} 
